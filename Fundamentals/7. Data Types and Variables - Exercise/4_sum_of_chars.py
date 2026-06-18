9b522412697cccbdbce48dbffda6d4e6aebd6190
arrisingme <arrisingme@gmail.com>
2026-06-18 11:07:27 +0300

    Add files via upload

---
+++ Fundamentals/7. Data Types and Variables - Exercise/4_sum_of_chars.py
@@ -0,0 +1,8 @@
+number_of_lines = int(input())
+total_sum = 0
+
+for char in range(number_of_lines):
+    current_char = input()
+    total_sum += ord(current_char)
+
+print(f"The sum equals: {total_sum}")
